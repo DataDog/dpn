32af919575b02d307cea3516e723d0e4eb5f6ace
Jarre Nejatyab <jarre.nejatyab@datadoghq.com>
2022-07-06 00:09:42 +1000

    Add help flag
    Added help flag
    Added some arguments validation

--- utils/SecretManager/src/secretBackendCommand.go
+++ utils/SecretManager/src/secretBackendCommand.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -12,17 +13,44 @@ import (
 
 func main() {
 
-	secrets := readSecretInput()
+	// help flag
+	needHelp()
 
+	checkArguments(2)
+
+	// reading secrect values based on the input secret provider
 	var output map[string]map[string]string
 	switch cloud := os.Args[1]; cloud {
 	case "aws":
-		output = awsSecretManager.GetSecrets(os.Args[2], os.Args[3], secrets.Secrets)
+		checkArguments(4)
+		output = awsSecretManager.GetSecrets(os.Args[2], os.Args[3], readSecretInput().Secrets)
 	case "azure":
-	 	output = azureSecretManager.GetSecrets(os.Args[2], secrets.Secrets)
+		checkArguments(3)
+	 	output = azureSecretManager.GetSecrets(os.Args[2], readSecretInput().Secrets)
+	default:
+        fmt.Println("no valid secret provider is given, use -h flag for more information.")
+		return
 	}
 
+	// writing the secret values to std output
 	fmt.Print(string(toJason(output)))
+}
+
+func needHelp() {
+	helpPtr := flag.Bool("h", false, "help")
+	flag.Parse()
+
+	if *helpPtr {
+		fmt.Print("First argument should be secret provider e.g azure or aws. Second argument should be secret name. if aws is chosen the third parameter should be the aws region.")
+		os.Exit(1)
+	}
+}
+
+func checkArguments(x int) {
+	if len(os.Args) < x {
+		fmt.Fprintf(os.Stderr, "valid arguments are required, use -h flag for more information.")
+		os.Exit(1)
+	}
 }
 
 func toJason(secretData map[string]map[string]string) []byte {
