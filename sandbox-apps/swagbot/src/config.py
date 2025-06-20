dbbe803bafa504425b2d187fd042cf32d9dff411
lucarelli111 <alessandro.lucarelli@datadoghq.com>
2025-06-20 14:52:33 -0400

    Changes: -Cleanup repo -Cpdated README -Updated LLM Spans to Auto-instrumentation instead of In-Code instrumentation - Updated UI look/feel - Updated Interaction script - Added Hallucinatios Generation

--- sandbox-apps/swagbot/src/config.py
+++ sandbox-apps/swagbot/src/config.py
@@ -8,16 +8,22 @@ class Config:
     DD_SITE = os.environ.get("DD_SITE", "datadoghq.com")
     DD_API_KEY = os.environ.get("DD_API_KEY")
     DD_LLMOBS_AGENTLESS_ENABLED = os.environ.get("DD_LLMOBS_AGENTLESS_ENABLED", False)
+    
+    # Datadog RUM Configs:
+    DD_APPLICATION_ID = os.environ.get("DD_APPLICATION_ID")
+    DD_CLIENT_TOKEN = os.environ.get("DD_CLIENT_TOKEN")
+    DD_ENV = os.environ.get("DD_ENV", "development")
    
     # LLM Configs:
-    LLM_TYPE = os.environ.get("LLM_TYPE", "OPEN_AI")
+    LLM_TYPE = os.environ.get("LLM_TYPE", "GEMINI")
     OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-    MODEL = os.environ.get("MODEL", "gpt-4o")
-    OPENAI_SYS_INSTRUCTIONS = os.environ.get("OPENAI_SYS_INSTRUCTIONS", "resources/openai-system-prompt.txt")
-    GCP_MODEL_ID = os.environ.get("GCP_MODEL_ID", "gemini-1.5-pro-002")
-    GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "datadog-sandbox")
+    MODEL_ID = os.environ.get("MODEL_ID", "gemini-1.5-pro-002") # openAi model id: gpt-4o
+    CATEGORIZATION_MODEL_ID = os.environ.get("CATEGORIZATION_MODEL_ID")  # If not set, will use default MODEL_ID
+    MODEL_SYS_INSTRUCTIONS = os.environ.get("GCP_SYS_INSTRUCTIONS", "resources/gemini-system-prompt.txt") # openAi system prompt location: resources/openai-system-prompt.txt
+
+    GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "datadog-partner-network")
     GCP_LLM_LOCATION = os.environ.get("GCP_LLM_LOCATION", "us-central1")
-    GCP_SYS_INSTRUCTIONS = os.environ.get("GCP_SYS_INSTRUCTIONS", "resources/gemini-system-prompt.txt")
+
     FLASK_HOST = os.environ.get("FLASK_HOST", "127.0.0.1")
     FLASK_PORT = int(os.environ.get("FLASK_PORT", 3000))
     REPLAY_PATH = os.environ.get("REPLAY_PATH","resources/replay_data")
